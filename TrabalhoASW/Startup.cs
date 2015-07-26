01d2ac389c6e5c692a84ffc3f4b544ab1eda9a17
fernandotnl <fernandotnl@hotmail.com>
2015-07-26 17:49:34 -0300

    Criando projeto

---
+++ TrabalhoASW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrabalhoASW.Startup))]
+namespace TrabalhoASW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
