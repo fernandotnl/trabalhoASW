6ab2a6debb2dd8cfbc3af48bf2e0fd17fb79443a
RaphaelPoeiras <raphaelpoeiras@gmail.com>
2015-08-16 22:36:11 -0300

    .

--- TrabalhoASW/Controllers/Testes/AlunoTest.cs
+++ TrabalhoASW/Controllers/Testes/AlunoTest.cs
@@ -10,26 +10,5 @@ namespace TrabalhoASW.Controllers.Testes
 {
     [TestFixture]
     public class AlunoTest
-    {
-        [Test]
-        public void criaAlunoTest()
-        {
-            UnidadeDeTrabalho unidadeDeTrabalho = new UnidadeDeTrabalho();
-            Models.Curso curso = new Models.Curso();
-            Models.Pessoa pessoa = new Models.Pessoa();
-            AlunoBusiness alunoBusiness = new AlunoBusiness(unidadeDeTrabalho);
-
-            Assert.Throws<Exception>(() => alunoBusiness.criaAluno(curso, pessoa));
-        }
-
-        [Test]
-        public void persisteAlunos()
-        {
-            UnidadeDeTrabalho unidadeDeTrabalho = new UnidadeDeTrabalho();
-            AlunoBusiness alunoBusiness = new AlunoBusiness(unidadeDeTrabalho);
-            List<Models.Aluno> aluno = new List<Models.Aluno>();
-
-            Assert.Throws<Exception>(() => alunoBusiness.persisteAlunos(aluno));
-        }
-    }
+    { }
 }
--- TrabalhoASW/Controllers/Testes/AvaliacaoTest.cs
+++ TrabalhoASW/Controllers/Testes/AvaliacaoTest.cs
@@ -11,24 +11,5 @@ namespace TrabalhoASW.Controllers.Testes
     [TestFixture]
     public class AvaliacaoTest
     {
-        [Test]
-        public void criarAvaliacao()
-        {
-            UnidadeDeTrabalho unidadeDeTrabalho = new UnidadeDeTrabalho();
-            AvaliacaoBusiness avaliacaoBusiness = new AvaliacaoBusiness(unidadeDeTrabalho);
-            Models.Turma turma = new Models.Turma();
-
-            Assert.Throws<Exception>(() => avaliacaoBusiness.criaAvaliacao("", turma));
-        }
-
-        [Test]
-        public void persisteAvaliacoes()
-        {
-            UnidadeDeTrabalho unidadeDeTrabalho = new UnidadeDeTrabalho();
-            AvaliacaoBusiness avaliacaoBusiness = new AvaliacaoBusiness(unidadeDeTrabalho);
-            List<Models.Avaliacao> avaliacao = new List<Models.Avaliacao>();
-
-            Assert.Throws<Exception>(() => avaliacaoBusiness.persisteAvaliacoes(avaliacao));
-        }
     }
 }
